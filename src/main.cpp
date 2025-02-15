82674f5c198202f5183445c19fcc0b65119be146
AmirrezaDaei <76740021+AmirrezaDaei@users.noreply.github.com>
2025-02-15 20:37:41 +0330

    EmpireAlwaysStrikesBack

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
+#include <iostream>
+using namespace std;
+
+string decrypt(string message, int key);
+string encrypt(string message, int key);
+
 int main() {
-    return 0;
+    string message, output, operation;
+    int key;
+    cin >> message;
+    cin >> key >> operation;
+    if(operation == "Encrypt") {
+        cout << encrypt(message, key);
+    }
+    else if(operation == "Decrypt") {
+        cout << decrypt(message, key);
+    }
+}
+
+string encrypt(string message, int key) {
+    for(int i = 0; i < message.length(); i++) {
+        if(int(message[i]) >= 65 && int(message[i]) <= 90) {
+            message[i] = char((message[i] + key - 65) % 26 + 65);
+        }
+        else if(int(message[i]) >= 97 && int(message[i]) <= 122) {
+            message[i] = char((message[i] + key - 97) % 26 + 97);
+        }
+    }
+    return message;
+}
+
+string decrypt(string message, int key) {
+    message = encrypt(message, 26 - key);
+    return message;
 }
